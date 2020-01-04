790b663a35b114fc54fb5903ef7ec4fd8d82e686
supreme-chocomint <59235097+supreme-chocomint@users.noreply.github.com>
2020-01-03 22:53:21 -0700

    initial: plot bands vs age/region/gender as bar graphs

---
+++ constants.py
@@ -0,0 +1,70 @@
+# Single response columns/questions
+# Duplicate names have indexes appended by Pandas
+REGION = "Where are you from?"
+GENDER = "What is your gender?.1"
+AGE = "How old are you?.1"
+PLAY_STYLE = "What is your preferred method of playing?"
+OTHER_GAMES_IDOL = "Do you play other idol-like games?"
+OTHER_GAMES_RHYTHM = "Do you play other rhythm games?"
+
+# Multi-response columns/questions
+BANDS_MUSIC = "Which bands are your favorite based on music? (You may choose up to 2)"
+BANDS_CHARA = "Which bands are your favorite based on members? (You may choose up to 2)"
+CHARACTERS = "Who are your favorite girls? (You may choose up to 5)"
+CHARACTER_REASONS = "What do you like most about your best girls?"
+SONGS_ORIGINAL = "What are your favorite original songs in the franchise? (You may choose up to 7)"
+SONGS_COVER = "What are your favorite cover songs? (You may choose up to 7)"
+FRANCHISE_PARTICIPATION = "What do you mainly participate in the BanG Dream! franchise?"
+
+# Values/answers
+NO_RESPONSE = "Prefer not to say"
+ALL_BANDS = [
+    "Poppin'Party",
+    "Roselia",
+    "RAISE A SUILEN",
+    "Afterglow",
+    "Pastel*Palettes",
+    "Hello, Happy World!",
+    "Glitter*Green"
+]
+ALL_CHARACTERS = [
+    "Toyama Kasumi (Poppin'Party - Vocals/Guitar)",
+    "Hanazono Tae (Poppin'Party - Lead Guitar)",
+    "Ushigome Rimi (Poppin'Party - Bass)",
+    "Yamabuki Saaya (Poppin'Party - Drums)",
+    "Ichigaya Arisa (Poppin'Party - Keyboard)",
+    "Minato Yukina (Roselia - Vocals)",
+    "Hikawa Sayo (Roselia - Guitar)",
+    "Imai Lisa (Roselia - Bass)",
+    "Udagawa Ako (Roselia - Drums)",
+    "Shirokane Rinko (Roselia - Keyboard)",
+    "Wakana \"LAYER\" Rei (RAS - Vocals/Bass)",
+    "Asahi \"LOCK\" Rokka (RAS - Guitar)",
+    "Satou \"MASKING\" Matsuki (RAS - Drums)",
+    "Nyubara \"PAREO\" Reona (RAS - Keyboard)",
+    "Tamade \"CHU2\" Chiyu (RAS - DJ)",
+    "Mitake Ran (Afterglow - Vocals/Guitar)",
+    "Aoba Moca (Afterglow - Lead Guitar)",
+    "Uehara Himari (Afterglow - Bass)",
+    "Udagawa Tomoe (Afterglow - Drums)",
+    "Hazawa Tsugumi (Afterglow - Keyboard)",
+    "Maruyama Aya (Pastel*Palettes - Vocals)",
+    "Hikawa Hina (Pastel*Palettes - Guitar)",
+    "Shirasagi Chisato (Pastel*Palettes - Bass)",
+    "Yamato Maya (Pastel*Palettes - Drums)",
+    "Wakamiya Eve (Pastel*Palettes - Keyboard)",
+    "Tsurumaki Kokoro (Hello, Happy World! - Vocals)",
+    "Seta Kaoru (Hello, Happy World! - Guitar)",
+    "Kitazawa Hagumi (Hello, Happy World! - Bass)",
+    "Matsubara Kanon (Hello, Happy World! - Drums)",
+    "Okusawa Misaki/Michelle (Hello, Happy World! - DJ)",
+    "Ushigome Yuri (Glitter*Green - Vocals/Guitar)",
+    "Uzawa Rii (Glitter*Green - Bass)",
+    "Nijukki Hinako (Glitter*Green - Drums)",
+    "Wanibe Nanana (Glitter*Green - Keyboard)",
+    "Toyama Asuka (Kasumi's Little Sister)",
+    "Umino Natsuki (CHiSPA Vocal + Guitarist | Saaya's friend)",
+    "Tsuzuki Shisen (SPACE Owner)",
+    "Tsukishima Marina (CiRCLE Staff)",
+    "Nono Yoshiko (Galaxy manager)"
+]
